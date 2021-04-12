1b5feaf370067dba84045ba27492f64cdc324c60
PeaWarrior <jacksonchen171@gmail.com>
2021-04-12 03:04:30 -0400

    add post to /login

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -18,7 +18,10 @@ def index():
   ]
   return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
   form = LoginForm()
+  if form.validate_on_submit():
+    flash(f'Login requested for {form.username.data}')
+    return redirect('/index')
   return render_template('login.html', title='Sign In', form=form)

687dcb041136460024c8b96c1fd867cb8293be2e
PeaWarrior <jacksonchen171@gmail.com>
2021-04-12 03:08:46 -0400

    add flash messages to base.html

--- app/routes.py
+++ app/routes.py
@@ -22,6 +22,6 @@ def index():
 def login():
   form = LoginForm()
   if form.validate_on_submit():
-    flash(f'Login requested for {form.username.data}')
+    flash(f'Login requested for user {form.username.data}, remember_me {form.remember_me.data}')
     return redirect('/index')
   return render_template('login.html', title='Sign In', form=form)
